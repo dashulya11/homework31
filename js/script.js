566bf39da608854e888668916dc49814289b3c93
dashulya11 <91964833+dashulya11@users.noreply.github.com>
2022-01-11 23:23:03 +0200

    init

--- js/script.js
+++ js/script.js
@@ -2,14 +2,20 @@
 
 const arr1 = [1, 2, [3, 4]];
 const arr2 = ['alins', 'dans', ['sashas', 'deph']];
+const arr3 = 123;
 
 function myFlat(array) {
   let newArr = [];
   if (arguments.length > 1) {
     throw new Error('Function accepts only 1 argument, too much arguments provided');
   }
-  newArr = array.join().split(',');
-  return newArr;
+  if (Array.isArray(array)) {
+    newArr = array.join().split(',');
+    return newArr;
+  }
+  return 'it is not an array!';
+  
 }
 console.log(myFlat(arr1));
 console.log(myFlat(arr2));
+console.log(myFlat(arr3));
